ca451edc75682c265cb25780bc013581760f9a14
AnmolAqsa <165940283+AnmolAqsa@users.noreply.github.com>
2024-07-04 12:49:55 -0700

    Add files via upload
    Q23, 45 Projects

---
+++ main.js
@@ -0,0 +1,45 @@
+/*
+Q23. Conditional Tests: Write a series of conditional tests. Print a statement
+describing each test and your prediction for the results of each test. Your code
+should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+* Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+* Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+*/
+var person = "Male";
+console.log("Is person == 'Male'? I predict True.");
+console.log(person == "Male");
+var color = "Blue";
+console.log("Is color == 'Green'? I predict False");
+console.log(color == "Green");
+var thing = "Toy";
+console.log("Is thing == 'Toy'? I predict True.");
+console.log(thing == "Toy");
+var name1 = "Arman";
+console.log("Is name1 == 'Anmol'? I predict False");
+console.log(name1 == "Anmol");
+var dish = "Biryani";
+console.log("Is dish == 'Kabab'? I predict False");
+console.log(dish == "Kabab");
+var num = "26";
+console.log("Is num == '26'? I predict True.");
+console.log(num == "26");
+var gender = "Female";
+console.log("Is gender == 'Female'? I predict True.");
+console.log(gender == "Female");
+var place = "Hospital";
+console.log("Is place == 'Park'? I predict False");
+console.log(place == "Park");
+var transport = "Car";
+console.log("Is transport == 'Car'? I predict True.");
+console.log(transport == "Car");
+var language = "TypeScript";
+console.log("Is language == 'Pathon'? I predict False");
+console.log(language == "Pathon");
---
+++ main.ts
@@ -0,0 +1,55 @@
+/*
+Q23. Conditional Tests: Write a series of conditional tests. Print a statement
+describing each test and your prediction for the results of each test. Your code
+should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+* Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+* Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+*/
+
+let person = "Male";
+console.log("Is person == 'Male'? I predict True.");
+console.log(person == "Male");
+
+let color = "Blue";
+console.log("Is color == 'Green'? I predict False");
+console.log(color == "Green");
+
+let thing = "Toy";
+console.log("Is thing == 'Toy'? I predict True.");
+console.log(thing == "Toy");
+
+let name1 = "Arman";
+console.log("Is name1 == 'Anmol'? I predict False");
+console.log(name1 == "Anmol");
+
+let dish = "Biryani";
+console.log("Is dish == 'Kabab'? I predict False");
+console.log(dish == "Kabab");
+
+let num = "26";
+console.log("Is num == '26'? I predict True.");
+console.log(num == "26");
+
+let gender = "Female";
+console.log("Is gender == 'Female'? I predict True.");
+console.log(gender == "Female");
+
+let place = "Hospital";
+console.log("Is place == 'Park'? I predict False");
+console.log(place == "Park");
+
+let transport = "Car";
+console.log("Is transport == 'Car'? I predict True.");
+console.log(transport == "Car");
+
+let language = "TypeScript";
+console.log("Is language == 'Pathon'? I predict False");
+console.log(language == "Pathon");
